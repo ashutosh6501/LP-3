362eec1b4ac81070d35fcf4a3633488a4140857e
ashutosh <93419471+ashutosh6501@users.noreply.github.com>
2022-11-23 12:49:45 +0530

    Update daa3.py

--- DAA/daa3.py
+++ DAA/daa3.py
@@ -1,46 +1,33 @@
-# Structure for an item which stores weight and
-# corresponding value of Item
 class Item:
-	def __init__(self, value, weight):
-		self.value = value
-		self.weight = weight
+  def __init__(self , profit , weight):
+    self.profit = profit
+    self.weight = weight
 
-# Main greedy function to solve problem
-def fractionalKnapsack(W, arr):
-
-	# sorting Item on basis of ratio
-	arr.sort(key=lambda x: (x.value/x.weight), reverse=True)
-
-	# Uncomment to see new order of Items with their ratio
-	for item in arr:
-		print(item.value, item.weight, round(item.value/item.weight,3))
-
-	# Result(value in Knapsack)
-	finalvalue = 0.0
-
-	# Looping through all Items
-	for item in arr:
-
-		# If adding Item won't overflow, add it completely
-		if item.weight <= W:
-			W -= item.weight
-			finalvalue += item.value
-
-		# If we can't add current Item, add fractional part of it
-		else:
-			finalvalue += item.value * W / item.weight
-			break
-		
-	# Returning final value
-	return finalvalue
+def fractionalKnapsack(capacity,arr):   
+  arr.sort(key=lambda obj : (obj.profit/obj.weight) ,reverse=True)
+  finalprofit=0.0
+  for item in arr:
+    if(item.weight < capacity):
+      capacity=capacity - item.weight
+      finalprofit=finalprofit + item.profit
+    else:
+      finalprofit=finalprofit+(item.profit*(capacity/item.weight))
+      break
+  return finalprofit
 
 
-# Driver's Code
-if __name__ == "__main__":
-	# Weight of Knapsack
-    W = 100
-    arr = [Item(10, 20), Item(20, 30), Item(30, 66), Item(40,40), Item(50,60)]
+if __name__=="__main__":
+  capacity=int(input("enter capacity: "))
+  n=int(input("enter the no of items: "))
+  arr=[]
+  for i in range(0,n):
+    profit = int(input("Enter profit: "))
+    weight = int(input("Enter weight: "))
+    temp = Item(profit,weight)
+    
+    arr.append(temp)
 
-	# Function call
-    print("Knapsack Capacity:", W)
-    print ("Maximum Profit:",fractionalKnapsack(W, arr))
+  # arr = [Item(24, 15), Item(15, 10), Item(25, 18)]
+  maxprofit=fractionalKnapsack(capacity,arr)
+  print(maxprofit)
+  
